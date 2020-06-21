abea01393cf1cb10c39a664f2da388f12ef29e3f
Kilipurt <proandrey15@gmail.com>
2020-06-21 23:39:35 +0300

    Refactor userDao

--- src/main/java/com/findme/dao/UserDAO.java
+++ src/main/java/com/findme/dao/UserDAO.java
@@ -9,26 +9,12 @@ import java.util.List;
 
 @Repository
 public interface UserDAO extends JpaRepository<User, Long> {
-//    private static final String GET_FRIENDS = "SELECT U.* FROM USER U, RELATIONSHIP R WHERE " +
-//            "(R.USER_FROM = :userId AND U.ID = R.USER_TO) OR (R.USER_FROM = U.ID AND R.USER_TO = :userId) AND " +
-//            "R.STATUS = 'FRIENDS'";
-
     @Query(value = "SELECT * FROM USER WHERE PHONE = ?1", nativeQuery = true)
     User getUserByPhone(String phone);
 
-//    public List<User> getFriends(long userId) throws InternalServerError {
-//        logger.info("UserDAO getFriends method. Selecting friends of user " + userId);
-//
-//        try {
-//            Query query = getEntityManager().createNativeQuery(GET_FRIENDS, User.class);
-//            query.setParameter("userId", userId);
-//
-//            return query.getResultList();
-//        } catch (Exception e) {
-//            logger.error("Getting is failed");
-//            throw new InternalServerError("Getting is failed");
-//        }
-//    }
+//    @Query(value = "SELECT U.* FROM USER U, RELATIONSHIP R WHERE (R.USER_FROM = :userId AND U.ID = R.USER_TO) OR" +
+//            " (R.USER_FROM = U.ID AND R.USER_TO = :userId) AND R.STATUS = 'FRIENDS'", nativeQuery = true)
+//    List<User> getFriends(long userId);
 
     @Query(value = "SELECT U.* FROM USER U JOIN RELATIONSHIP R ON U.ID = R.USER_FROM WHERE R.USER_TO = ?1 AND " +
             "R.STATUS = 'REQUEST_SENT'", nativeQuery = true)
